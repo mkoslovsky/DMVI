0db9d78904ec2dca5d68da4b0c0c2ad831aff8d4
Matthew Koslovsky <mkoslovsky12@gmail.com>
2020-04-03 14:12:02 -0500

    Test connection

--- src/rcpparma_hello_world.cpp
+++ src/rcpparma_hello_world.cpp
@@ -19,7 +19,7 @@ arma::mat rcpparma_hello_world() {
     arma::mat m1 = arma::eye<arma::mat>(3, 3);
     arma::mat m2 = arma::eye<arma::mat>(3, 3);
 	                     
-    return m1 + 36 * (m1 + m2);
+    return m1 + 3 * (m1 + m2);
 }
 
 
